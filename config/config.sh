5a5bec91db89ea1cb4a2ba76226512fadceaf6e5
Luigi Calori <luigi.calori@gmail.com>
2017-03-30 15:40:15 +0200

    added config files and repository packages

---
+++ config/config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+spack repo add @{DEPLOY_ROOTPATH}/repo --scope site
+#spack  compiler find --scope site
+#spack info rcm
+#spack install --verbose py-flake8
+#spack find
+#spack module  rm -y
+#spack module refresh -y
